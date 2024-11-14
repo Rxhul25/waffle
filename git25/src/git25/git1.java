3f4b93794c50d96dbc529e58922b5ad00baf98f3
Rxhul25 <141322805+Rxhul25@users.noreply.github.com>
2024-11-14 17:26:27 +0530

    Update git1.java

--- git25/src/git25/git1.java
+++ git25/src/git25/git1.java
@@ -1,5 +1,15 @@
 package practice;
-
+import java.util.*;
 public class git1 {
-
+	public static float si(float p,float r,float t) {
+		return (p*r*t)/100;
+	}
+	
+	public static float discount(float p) {
+		 return p - (p * 0.1f);
+	}
+	public static int arrs(int[] arr) {
+		Arrays.sort(arr);
+		return arr[0];
+	}
 }
