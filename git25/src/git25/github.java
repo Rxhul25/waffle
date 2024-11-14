a2f2726d3662f23106ce392e4652b8a2a6d75b7e
Rxhul25 <141322805+Rxhul25@users.noreply.github.com>
2024-11-14 17:19:53 +0530

    Update github.java

--- git25/src/git25/github.java
+++ git25/src/git25/github.java
@@ -1,5 +1,20 @@
 package git25;
 
 public class github {
+  import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class git {
+
+	
+	void test() {
+		git1 g=new git1();
+		int[] arr= {1,2,3,4,5};
+		Assertions.assertEquals(10, g.si(20, 40, 4));
+		Assertions.assertEquals(20, g.discount(30));
+		Assertions.assertEquals(10, g.arrs(arr));
+	}
+
+}
+
 
 }
